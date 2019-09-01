e7e0ca21a74daa283868cf05d5983bbd36f35cab
Stephon Barrett <exdsxt@gmail.com>
2019-09-01 17:19:24 -0400

    Ensuring that the enviroment works

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    </head>
+<!-- http://localhost/CMS-in-PHP/index.php -->
 
-    <body>
-    </body>
-</html>
+<?php include("views/header.php") ?>
+    <title>Black Yelp</title>
+</head>
+
+<?php include("views/nav.php") ?>
+
+<body>
+    <h1>Hello World</h1>
+<?php include("views/footer.php") ?>
